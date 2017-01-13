ba119b5c240bdf344aa8fb3d549dade51cfbecda
ioncrash <ioncrash@gmail.com>
2017-01-13 09:36:37 -0500

    deploy?

---
+++ assets/VJBooth.js
@@ -0,0 +1,315 @@
+"use strict";
+
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+define('VJBooth/app', ['exports', 'ember', 'VJBooth/resolver', 'ember-load-initializers', 'VJBooth/config/environment'], function (exports, _ember, _VJBoothResolver, _emberLoadInitializers, _VJBoothConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _VJBoothConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _VJBoothConfigEnvironment['default'].podModulePrefix,
+    Resolver: _VJBoothResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _VJBoothConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('VJBooth/application/adapter', ['exports', 'VJBooth/config/environment', 'active-model-adapter'], function (exports, _VJBoothConfigEnvironment, _activeModelAdapter) {
+  exports['default'] = _activeModelAdapter['default'].extend({
+    host: _VJBoothConfigEnvironment['default'].apiHost
+  });
+});
+define('VJBooth/application/serializer', ['exports', 'active-model-adapter'], function (exports, _activeModelAdapter) {
+  exports['default'] = _activeModelAdapter.ActiveModelSerializer.extend({});
+});
+define("VJBooth/application/template", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "sfunAF0j", "block": "{\"statements\":[[\"open-element\",\"h1\",[]],[\"flush-element\"],[\"text\",\" Welcome to Ember! \"],[\"close-element\"],[\"text\",\"\\n\\n\"],[\"append\",[\"unknown\",[\"outlet\"]],false],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "VJBooth/application/template.hbs" } });
+});
+define('VJBooth/controllers/array', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('VJBooth/controllers/object', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('VJBooth/helpers/app-version', ['exports', 'ember', 'VJBooth/config/environment'], function (exports, _ember, _VJBoothConfigEnvironment) {
+  exports.appVersion = appVersion;
+  var version = _VJBoothConfigEnvironment['default'].APP.version;
+
+  function appVersion() {
+    return version;
+  }
+
+  exports['default'] = _ember['default'].Helper.helper(appVersion);
+});
+define('VJBooth/helpers/pluralize', ['exports', 'ember-inflector/lib/helpers/pluralize'], function (exports, _emberInflectorLibHelpersPluralize) {
+  exports['default'] = _emberInflectorLibHelpersPluralize['default'];
+});
+define('VJBooth/helpers/singularize', ['exports', 'ember-inflector/lib/helpers/singularize'], function (exports, _emberInflectorLibHelpersSingularize) {
+  exports['default'] = _emberInflectorLibHelpersSingularize['default'];
+});
+define('VJBooth/index/route', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Route.extend({
+    volume: 10,
+    //
+    actions: {
+      changeVolume: function changeVolume() {
+        console.log(this.get('volume'));
+      }
+    }
+  });
+});
+define("VJBooth/index/template", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "uqBKJHa2", "block": "{\"statements\":[[\"open-element\",\"form\",[]],[\"modifier\",[\"action\"],[[\"get\",[null]],\"changeVolume\"]],[\"flush-element\"],[\"text\",\"\\n  \"],[\"append\",[\"helper\",[\"input\"],null,[[\"value\",\"placeholder\"],[[\"get\",[\"volume\"]],\"volume\"]]],false],[\"text\",\"\\n  \"],[\"open-element\",\"button\",[]],[\"static-attr\",\"type\",\"submit\"],[\"flush-element\"],[\"text\",\"submit\"],[\"close-element\"],[\"text\",\"\\n\"],[\"close-element\"],[\"text\",\"\\n\"],[\"append\",[\"unknown\",[\"outlet\"]],false],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "VJBooth/index/template.hbs" } });
+});
+define("VJBooth/initializers/active-model-adapter", ["exports", "active-model-adapter", "active-model-adapter/active-model-serializer"], function (exports, _activeModelAdapter, _activeModelAdapterActiveModelSerializer) {
+  exports["default"] = {
+    name: 'active-model-adapter',
+    initialize: function initialize() {
+      var application = arguments[1] || arguments[0];
+      application.register('adapter:-active-model', _activeModelAdapter["default"]);
+      application.register('serializer:-active-model', _activeModelAdapterActiveModelSerializer["default"]);
+    }
+  };
+});
+define('VJBooth/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'VJBooth/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _VJBoothConfigEnvironment) {
+  var _config$APP = _VJBoothConfigEnvironment['default'].APP;
+  var name = _config$APP.name;
+  var version = _config$APP.version;
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(name, version)
+  };
+});
+define('VJBooth/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('VJBooth/initializers/data-adapter', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `data-adapter` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'data-adapter',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define('VJBooth/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
+
+  /*
+  
+    This code initializes Ember-Data onto an Ember application.
+  
+    If an Ember.js developer defines a subclass of DS.Store on their application,
+    as `App.StoreService` (or via a module system that resolves to `service:store`)
+    this code will automatically instantiate it and make it available on the
+    router.
+  
+    Additionally, after an application's controllers have been injected, they will
+    each have the store made available to them.
+  
+    For example, imagine an Ember.js application with the following classes:
+  
+    App.StoreService = DS.Store.extend({
+      adapter: 'custom'
+    });
+  
+    App.PostsController = Ember.Controller.extend({
+      // ...
+    });
+  
+    When the application is initialized, `App.ApplicationStore` will automatically be
+    instantiated, and the instance of `App.PostsController` will have its `store`
+    property set to that instance.
+  
+    Note that this code will only be run if the `ember-application` package is
+    loaded. If Ember Data is being used in an environment other than a
+    typical application (e.g., node.js where only `ember-runtime` is available),
+    this code will be ignored.
+  */
+
+  exports['default'] = {
+    name: 'ember-data',
+    initialize: _emberDataSetupContainer['default']
+  };
+});
+define('VJBooth/initializers/export-application-global', ['exports', 'ember', 'VJBooth/config/environment'], function (exports, _ember, _VJBoothConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_VJBoothConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
+      var value = _VJBoothConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_VJBoothConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete theGlobal[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('VJBooth/initializers/injectStore', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `injectStore` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'injectStore',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define('VJBooth/initializers/store', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `store` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'store',
+    after: 'ember-data',
+    initialize: function initialize() {}
+  };
+});
+define('VJBooth/initializers/transforms', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `transforms` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'transforms',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define("VJBooth/instance-initializers/ember-data", ["exports", "ember-data/-private/instance-initializers/initialize-store-service"], function (exports, _emberDataPrivateInstanceInitializersInitializeStoreService) {
+  exports["default"] = {
+    name: "ember-data",
+    initialize: _emberDataPrivateInstanceInitializersInitializeStoreService["default"]
+  };
+});
+define('VJBooth/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('VJBooth/router', ['exports', 'ember', 'VJBooth/config/environment'], function (exports, _ember, _VJBoothConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _VJBoothConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('VJBooth/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('VJBooth/config/environment', ['ember'], function(Ember) {
+  var prefix = 'VJBooth';
+/* jshint ignore:start */
+
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+/* jshint ignore:end */
+
+});
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+if (!runningTests) {
+  require("VJBooth/app")["default"].create({"name":"VJBooth","version":"0.0.1+e5b7e99d"});
+}
+
+/* jshint ignore:end */
+//# sourceMappingURL=VJBooth.map
